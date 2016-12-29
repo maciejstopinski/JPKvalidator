048881fba0910513fe9531df3aa04245345a381d
Maciej Stopiński <maciej.stopinski@hotmail.com>
2016-12-29 22:42:45 +0100

    test

--- JPKvalidator/Form1.Designer.cs
+++ JPKvalidator/Form1.Designer.cs
@@ -35,7 +35,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(703, 286);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
             this.Text = "JPK validator";
